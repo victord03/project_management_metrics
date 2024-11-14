6c67a3b2e14d06b0dab6a9c899d748cfd3e01214
Kaklamanis <victor.kaklamanis@accenture.com>
2024-11-14 15:58:59 +0200

    Initial setup.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+PLANNED_START = datetime.date
+DURATION = int()  # in days
+
+def calculate_planned_progress():
+    ...
+
+
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
