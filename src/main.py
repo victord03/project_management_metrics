ec35dd487aeb25233b88e646af4e0d7bb7fc4cb4
Kaklamanis <victor.kaklamanis@accenture.com>
2024-11-14 16:16:46 +0200

    Starting on planned progress calculations.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 
-PLANNED_START = datetime.date
-DURATION = int()  # in days
 
-def calculate_planned_progress():
-    ...
-
-
+def calculate_planned_progress(planned_start: datetime.date, duration: int, reporting_date: datetime.date):
+    return duration - (planned_start - reporting_date)
 
 
 def main():
-    ...
+
+    planned_start = datetime(day=12, month=11, year=2024)
+    current_day = datetime(day=12, month=11, year=2024)
+    duration = 4
+
+    planned_progress = calculate_planned_progress(planned_start, duration, current_day)
+
+    print(planned_progress)
 
 
 if __name__ == "__main__":
